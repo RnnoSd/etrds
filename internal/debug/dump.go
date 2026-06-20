d609c551596458f72462fbec08827445b3d40489
RnnoSd <renatocvz2025@gmail.com>
2026-06-19 20:47:52 -0500

    add select_stmt to the parsing using sqlc implementation(review needed)

---
+++ internal/debug/dump.go
@@ -0,0 +1,28 @@
+package debug
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+
+	"github.com/RnnoSd/etrds/internal/sqlcdebug"
+)
+
+// Active reports whether SQLCDEBUG had any value set at startup. It
+// remains a global so unrelated debug-spew sites that don't tie to a
+// specific setting can gate their output on "is debug mode on at all".
+var Active = sqlcdebug.Any()
+
+func Dump(n ...any) {
+	if Active {
+		spew.Dump(n)
+	}
+}
+
+func DumpAsJSON(a any) {
+	if Active {
+		out, _ := json.MarshalIndent(a, "", "  ")
+		fmt.Printf("%s\n", out)
+	}
+}
